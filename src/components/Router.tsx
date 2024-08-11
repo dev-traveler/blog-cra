c79100afc89115fee4ea056d088ca6d9a9672d9c
Jaehyun Kim <avenj.dev@gmail.com>
2024-08-11 20:41:02 +0900

    refactor: Update Router component to conditionally render routes based on authentication status

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from 'pages/home';
 import PostlistPage from 'pages/posts';
@@ -9,17 +10,30 @@ import LoginPage from 'pages/login';
 import SignUpPage from 'pages/signup';
 
 function Router() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/posts" element={<PostlistPage />} />
-      <Route path="/posts/:id" element={<PostDetailPage />} />
-      <Route path="/posts/new" element={<NewPostPage />} />
-      <Route path="/posts/edit/:id" element={<EditPostPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="*" element={<Navigate replace to="/" />} />
+      {isAuthenticated ? (
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<PostlistPage />} />
+          <Route path="/posts/:id" element={<PostDetailPage />} />
+          <Route path="/posts/new" element={<NewPostPage />} />
+          <Route path="/posts/edit/:id" element={<EditPostPage />} />
+
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="*" element={<LoginPage />} />
+        </>
+      )}
     </Routes>
   );
 }
