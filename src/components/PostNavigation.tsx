0ce6a2a6971bd17b5fde399e7bd875b694d81036
Jaehyun Kim <avenj.dev@gmail.com>
2024-08-11 17:57:21 +0900

    feat: Add PostNavigation component

---
+++ src/components/PostNavigation.tsx
@@ -0,0 +1,10 @@
+function PostNavigation() {
+  return (
+    <div className="post__navigation">
+      <div className="post__navigation--active">전체</div>
+      <div>나의 글</div>
+    </div>
+  );
+}
+
+export default PostNavigation;
