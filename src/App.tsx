0aec640ef19ff7bbb6e8b9df79ce1af7d1e3ed68
Jaehyun Kim <avenj.dev@gmail.com>
2024-08-11 23:37:17 +0900

    refactor: Initialize App component with a Loader before authentication check

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,20 @@ import { ToastContainer } from 'react-toastify';
 import Router from './components/Router';
 
 import 'react-toastify/dist/ReactToastify.css';
+import Loader from 'components/Loader';
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(!!auth.currentUser);
 
   onAuthStateChanged(auth, (user) => {
     setIsAuthenticated(!!user);
+    setInit(true);
   });
 
   return (
     <>
-      <Router isAuthenticated={isAuthenticated} />;
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
       <ToastContainer />
     </>
   );
