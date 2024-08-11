cd69f1eb2428af30fc168781e61f5c2ef71fa59d
Jaehyun Kim <avenj.dev@gmail.com>
2024-08-11 17:57:53 +0900

    feat: Add PostDetail component

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,41 @@
+function PostDetail() {
+  return (
+    <>
+      <div className="post__detail">
+        <div className="post__box">
+          <div className="post__title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo neque,
+            minus, assumenda magni rerum perferendis.
+          </div>
+
+          <div className="post__profile-box">
+            <div className="post__profile" />
+            <div className="post__author-name">dev_traveler</div>
+            <div className="post__date">2024.8.11 일요일</div>
+          </div>
+
+          <div className="post__utils-box">
+            <div className="post__delete">삭제</div>
+            <div className="post__edit">수정</div>
+          </div>
+
+          <div className="post__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            dolore vero aut amet exercitationem! Sapiente delectus, officia vero
+            quam tenetur nemo iure reiciendis ipsa in cupiditate veritatis,
+            dolores laborum minima. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Ratione dolore vero aut amet exercitationem!
+            Sapiente delectus, officia vero quam tenetur nemo iure reiciendis
+            ipsa in cupiditate veritatis, dolores laborum minima. Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Ratione dolore vero aut
+            amet exercitationem! Sapiente delectus, officia vero quam tenetur
+            nemo iure reiciendis ipsa in cupiditate veritatis, dolores laborum
+            minima.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PostDetail;
