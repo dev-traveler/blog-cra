a023dd5315ca15dd0777ff68e71f5b18245113fa
Jaehyun Kim <avenj.dev@gmail.com>
2024-08-11 17:59:14 +0900

    feat: Add Header, PostList, Profile, and Footer components to ProfilePage

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,17 @@
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import PostList from '../../components/PostList';
+import Profile from '../../components/Profile';
+
 function ProfilePage() {
-  return <h1>Profile</h1>;
+  return (
+    <>
+      <Header />
+      <Profile />
+      <PostList />
+      <Footer />
+    </>
+  );
 }
 
 export default ProfilePage;
