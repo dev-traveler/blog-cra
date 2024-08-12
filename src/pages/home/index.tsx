4e0b94d73549ffe474c54acbd256d7f453006f93
Jaehyun Kim <avenj.dev@gmail.com>
2024-08-12 09:51:34 +0900

    feat: Add Carousel component to Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,11 +2,13 @@ import Header from 'components/Header';
 import PostNavigation from 'components/PostNavigation';
 import PostList from 'components/PostList';
 import Footer from 'components/Footer';
+import Carousel from 'components/Carousel';
 
 function Home() {
   return (
     <div>
       <Header />
+      <Carousel />
       <PostNavigation />
       <PostList />
       <Footer />
