3b173762a384fbcc7a534bced17ddd6b7d5e699f
Jaehyun Kim <avenj.dev@gmail.com>
2024-08-13 09:07:06 +0900

    feat: Add Firestore database initialization to firebaseApp.ts

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { FirebaseApp, getApp, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,5 +22,6 @@ try {
 }
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth };
+export { app, auth, db };
