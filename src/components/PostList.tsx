6a6b511f700fbe55081840ca37f7648d84691834
Jaehyun Kim <avenj.dev@gmail.com>
2024-08-14 06:54:35 +0900

    feat: Fetch and display posts from Firestore in PostList component

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,38 +1,62 @@
+import { collection, getDocs } from 'firebase/firestore';
+import { auth, db } from 'firebaseApp';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+interface Post {
+  id: string;
+  title: string;
+  email: string;
+  summary: string;
+  content: string;
+  createdAt: string;
+}
+
 function PostList() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const getPosts = async () => {
+    const data = await getDocs(collection(db, 'posts'));
+
+    data.forEach((doc) => {
+      const dataObj = { ...doc.data(), id: doc.id } as Post;
+      setPosts((prev) => [...prev, dataObj]);
+    });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <>
       <div className="post__list">
-        {[...Array(10)].map((_, index) => (
-          <div key={index} className="post__box">
-            <Link to={`/posts/${index}`}>
-              <div className="post__profile-box">
-                <div className="post__profile" />
-                <div className="post__author-name">dev_traveler</div>
-                <div className="post__date">2024.8.11 일요일</div>
-              </div>
-              <div className="post__title">게시글 {index}</div>
-              <div className="post__text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-                dolore vero aut amet exercitationem! Sapiente delectus, officia
-                vero quam tenetur nemo iure reiciendis ipsa in cupiditate
-                veritatis, dolores laborum minima. Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Ratione dolore vero aut amet
-                exercitationem! Sapiente delectus, officia vero quam tenetur
-                nemo iure reiciendis ipsa in cupiditate veritatis, dolores
-                laborum minima. Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Ratione dolore vero aut amet exercitationem!
-                Sapiente delectus, officia vero quam tenetur nemo iure
-                reiciendis ipsa in cupiditate veritatis, dolores laborum minima.
-              </div>
-              <div className="post__utils-box">
-                <div className="post__delete">삭제</div>
-                <div className="post__edit">수정</div>
-              </div>
-            </Link>
-          </div>
-        ))}
+        {posts.length === 0 ? (
+          <div className="post__no-post">게시글이 없습니다.</div>
+        ) : (
+          posts.map((post) => (
+            <div key={post.id} className="post__box">
+              <Link to={`/posts/${post.id}`}>
+                <div className="post__profile-box">
+                  <div className="post__profile" />
+                  <div className="post__author-name">{post.email}</div>
+                  <div className="post__date">{post.createdAt}</div>
+                </div>
+                <div className="post__title">{post.title}</div>
+                <div className="post__text">{post.content}</div>
+              </Link>
+
+              {post.email === auth.currentUser?.email && (
+                <div className="post__utils-box">
+                  <div className="post__delete">삭제</div>
+                  <Link to={`/posts/edit/${post.id}`} className="post__edit">
+                    수정
+                  </Link>
+                </div>
+              )}
+            </div>
+          ))
+        )}
       </div>
     </>
   );
