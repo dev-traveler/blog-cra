dc2650423416f5469124aa5f3253997d8dbd6b05
Jaehyun Kim <avenj.dev@gmail.com>
2024-08-18 17:59:22 +0900

    feat: Add ThemeContextProvider to App and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import 'react-toastify/dist/ReactToastify.css';
 
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { auth } from 'firebaseApp';
 import { onAuthStateChanged } from 'firebase/auth';
 import { ToastContainer } from 'react-toastify';
 
+import { ThemeContext } from 'context/ThemeContext';
 import Loader from 'components/Loader';
 import Router from 'components/Router';
 
 function App() {
+  const context = useContext(ThemeContext);
+
   const [init, setInit] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(!!auth.currentUser);
 
-  onAuthStateChanged(auth, (user) => {
-    setIsAuthenticated(!!user);
-    setInit(true);
-  });
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setIsAuthenticated(!!user);
+      setInit(true);
+    });
+  }, []);
 
   return (
-    <>
+    <div className={context.theme}>
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
       <ToastContainer />
-    </>
+    </div>
   );
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,24 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { BsSun, BsMoonFill } from 'react-icons/bs';
+import { ThemeContext } from 'context/ThemeContext';
 
 function Footer() {
+  const context = useContext(ThemeContext);
+
   return (
     <footer>
       <Link to="/posts/new">글쓰기</Link>
       <Link to="/posts">게시글</Link>
       <Link to="/profile">프로필</Link>
+      {context.theme === 'light' ? (
+        <BsSun className="footer__theme-btn" onClick={context.toggleTheme} />
+      ) : (
+        <BsMoonFill
+          className="footer__theme-btn"
+          onClick={context.toggleTheme}
+        />
+      )}
     </footer>
   );
 }
